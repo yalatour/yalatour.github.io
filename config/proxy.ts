6bda9aa8cbb949a614f7dfb0b0f24784dd8ca134
dmhashanwork <120355703+dmhashanwork@users.noreply.github.com>
2023-03-26 08:32:46 +0530

    initial testing

--- config/proxy.ts
+++ config/proxy.ts
@@ -12,10 +12,10 @@
 export default {
   // 如果需要自定义本地开发服务器  请取消注释按需调整
   // dev: {
-  //   // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+  //   // localhost:8000/api/** -> https://yalatour.lk/api/**
   //   '/api/': {
   //     // 要代理的地址
-  //     target: 'https://preview.pro.ant.design',
+  //     target: 'https://yalatour.lk',
   //     // 配置了这个可以从 http 代理到 https
   //     // 依赖 origin 的功能可能需要这个，比如 cookie
   //     changeOrigin: true,
@@ -27,7 +27,7 @@ export default {
    * @doc https://github.com/chimurai/http-proxy-middleware
    */
   test: {
-    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+    // localhost:8000/api/** -> https://yalatour.lk/api/**
     '/api/': {
       target: 'https://proapi.azurewebsites.net',
       changeOrigin: true,
